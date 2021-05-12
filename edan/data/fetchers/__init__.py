c671216203fa2568bc92e04734ff2266eebbe261
hotzsauce <githotz@gmail.com>
2021-05-12 18:17:45 -0500

    don't require API init

--- edan/data/fetchers/__init__.py
+++ edan/data/fetchers/__init__.py
@@ -5,13 +5,9 @@ from edan.data.fetchers.bea_fetcher import BeaFetcher
 from edan.data.fetchers.base import EdanFetcher
 
 fetchers_by_source = {}
-def add_source(source, api):
-	try:
-		_api = api()
-		fetchers_by_source[source] = _api
-	except KeyError:
-		# api key was not saved
-		pass
+def add_source(source, fetcher):
+	fetchers_by_source[source] = fetcher
+
 add_source('fred', FredFetcher)
 add_source('av', AlphaVantageFetcher)
 add_source('bea', BeaFetcher)
