e9d68e01d18f7f5219bece63081f95d3786e8b06
hotzsauce <githotz@gmail.com>
2021-05-22 20:28:45 -0500

    moved Component functionality to 'aggregates' dir

--- edan/nipa/components.py
+++ edan/nipa/components.py
@@ -5,153 +5,33 @@ module for NIPA components
 from __future__ import annotations
 
 from edan.accessors import CachedAccessor
-from edan.containers import (
-	CoreSeries,
-	CompoundStorage
-)
 
-from edan.delims import (
-	concat_codes,
-	contains
-)
+from edan.aggregates.series import Series
+from edan.aggregates.components import Component
 
-from edan.data.retrieve import retriever
-
-from edan.nipa.disaggregate import Disaggregator
 from edan.nipa.features import Contribution
 from edan.nipa.modifications import ModificationAccessor
 
 from edan.plotting.nipa import NIPAPlotAccessor
 
 
-class NIPASeries(CoreSeries):
+class NIPASeries(Series):
 
 	def __init__(
 		self,
-		code: str, dtype: str, obj: Component,
+		code: str, mtype: str, obj: Component,
 		*args, **kwargs
 	):
-		if code:
-			data, meta = retriever.retrieve(code, source=obj.source)
-			super().__init__(code, data)
+		super().__init__(code, source=obj.source)
 
-			self.meta = meta
-		else:
-			super().__init__(code)
-			self.meta = None
-
-		self.dtype = dtype
+		self.mtype = mtype
 		self.obj = obj
-
-
-	def __repr__(self):
-		return f"NIPASeries({self.code})"
 
 	# add accessor for functions of data
 	modify = CachedAccessor('modify', ModificationAccessor)
 
 
-class Component(CompoundStorage):
-	"""
-	a collection of NIPASeries that represents a single Component
-	"""
-
-	def __init__(
-		self,
-		code: str,
-		qcode: str = '',
-		pcode: str = '',
-		ncode: str = '',
-		rcode: str = '',
-		level: int = 0,
-		long_name: str = '',
-		short_name: str = '',
-		source: str = ''
-	):
-		# initialize CompoundStorage class with attribute names that hold data
-		super().__init__(fields=('quantity', 'price', 'nominal', 'real'))
-
-		# various codes for referencing the different data comprising this Component
-		self.code = code
-		self.qcode = qcode
-		self.pcode = pcode
-		self.ncode = ncode
-		self.rcode = rcode
-
-		# attributes about relations to other Components
-		self.level = level
-		self.subs = []
-		self.sups = ''
-
-		# display names & source api
-		self.long_name = long_name
-		self.short_name = short_name
-		self.source = source
-
-
-
-	def __getitem__(self, key: str):
-		if self.elemental:
-			raise ValueError(f"{repr(self)} is an elemental component")
-
-		if not isinstance(key, str):
-			raise TypeError(f"'key' must be a string")
-
-		subs = [s.code for s in self.subs]
-		try:
-			# assume `key` is the entire code of a subcomponent
-			idx = subs.index(key)
-			return self.subs[idx]
-
-		except ValueError:
-			# `key` is relative to the code of this component
-
-			full_key = concat_codes(self.code, key)
-			try:
-				idx = subs.index(full_key)
-				return self.subs[idx]
-			except ValueError:
-				raise KeyError(
-					f"{repr(key)} does not match a subcomponent of {repr(self)}"
-				) from None
-
-	def disaggregate(
-		self,
-		subs: Iterable[str] = [],
-		level: int = 0
-	):
-		"""
-		locate & return a list of subcomponents that can be further down the
-		subcomponent tree than just the Component objects immediately available
-		in the `subs` attribute. functionality for selecting based on `edan` code
-		and level relative to the current Component is provided
-
-		Parameters
-		----------
-		subs : list ( = [] )
-			an iterable of subcomponents to return. the elements of `subs` are
-			assumed to be (relative or absolute) `edan` codes
-		level : int ( = 0 )
-			the level, relative to the current Component, of subcomponents that
-			are to be returned. if a subcomponent has level `l`, with
-			l < level + self.level, and that subcomponent is elemental, it is also
-			included
-
-		Returns
-		-------
-		disaggregate : List[Component]
-		"""
-
-		# should raise Error or return None if this component is elemental
-
-		return Disaggregator(self, subs, level)
-
-	@property
-	def elemental(self):
-		"""return True if there are no subcomponents"""
-		if self.subs:
-			return False
-		return True
+class NIPAComponent(Component):
 
 	@property
 	def quantity(self):
@@ -163,7 +43,7 @@ class Component(CompoundStorage):
 		if hasattr(self, '_quantity'):
 			return self._quantity
 		else:
-			self._quantity = NIPASeries(self.qcode, 'quantity', self)
+			self._quantity = NIPASeries(self.quantity_code, 'quantity', self)
 			return self._quantity
 
 	@property
@@ -176,7 +56,7 @@ class Component(CompoundStorage):
 		if hasattr(self, '_price'):
 			return self._price
 		else:
-			self._price = NIPASeries(self.pcode, 'price', self)
+			self._price = NIPASeries(self.price_code, 'price', self)
 			return self._price
 
 	@property
@@ -189,7 +69,7 @@ class Component(CompoundStorage):
 		if hasattr(self, '_nominal'):
 			return self._nominal
 		else:
-			self._nominal = NIPASeries(self.ncode, 'nominal', self)
+			self._nominal = NIPASeries(self.nominal_code, 'nominal', self)
 			return self._nominal
 
 	@property
@@ -202,25 +82,8 @@ class Component(CompoundStorage):
 		if hasattr(self, '_real'):
 			return self._real
 		else:
-			self._real = NIPASeries(self.rcode, 'real', self)
+			self._real = NIPASeries(self.real_code, 'real', self)
 			return self._real
-
-	@classmethod
-	def from_registry(self, dct: dict):
-		return Component(
-			dct['code'],
-			dct['quantity'],
-			dct['price'],
-			dct['nominal'],
-			dct['real'],
-			dct['level'],
-			dct['long_name'],
-			dct['short_name'],
-			dct['source']
-		)
-
-	def __repr__(self):
-		return f"Component({self.code}, {self.level})"
 
 	# add accessor for plotting
 	plot = CachedAccessor('plot', NIPAPlotAccessor)
