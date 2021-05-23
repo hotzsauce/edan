6be39e5dd7b29d09dfcf3f2ffd07d61fe8498d03
hotzsauce <githotz@gmail.com>
2021-05-22 21:15:37 -0500

    added CPISeries & 'price' property

--- edan/cpi/core.py
+++ edan/cpi/core.py
@@ -1,10 +1,37 @@
 """
-
+module for CPI components
 """
 
+from edan.aggregates.series import Series
 from edan.aggregates.components import Component
+
+
+class CPISeries(Series):
+
+	def __init__(
+		self,
+		code: str, mtype: str, obj: Component,
+		*args, **kwargs
+	):
+		super().__init__(code, source=obj.source)
+
+		self.mtype = mtype
+		self.obj = obj
 
 
 class CPIComponent(Component):
 
 	mtypes = ['price']
+
+	@property
+	def price(self):
+		"""
+		access the CPISeries corresponding to the price index of this Component.
+		It's implemented this way, and not with @cached_property, because I don't
+		think the decorator could tell if attributes of the CPISeries were changed
+		"""
+		if hasattr(self, '_price'):
+			return self._price
+		else:
+			self._price = CPISeries(self.price_code, 'price', self)
+			return self._price
